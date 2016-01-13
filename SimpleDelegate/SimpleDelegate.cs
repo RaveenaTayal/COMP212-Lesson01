806451b96351403b5293f6ad31e1a4a9fd21cd38
RaveenaTayal <ravhomw123@gmail.com>
2016-01-13 17:07:18 -0500

    Initial commit

---
+++ SimpleDelegate/SimpleDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    class SimpleDelegate
+    {
+        public delegate void Del(string message);
+        static void Main(string[] args)
+        {
+            //Instantiate the delegate
+            Del handler = DelegateMethod;
+
+            Del handler2 = DelegateMethod2;
+
+            //Call the delegate
+            handler("Hello World");
+
+            handler2.Invoke("This is through invoke: Hello World");
+
+            Console.ReadKey();
+        }
+
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void DelegateMethod2(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
